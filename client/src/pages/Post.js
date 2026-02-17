2bb044f7a37cbc9245ae67d762516219aaf4a724
Pieter-BasdePottere <pbdepotter@hotmail.com>
2026-02-17 20:35:27 +0100

    third commit

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from "axios";  
 
 function Post() {
     let { id } = useParams(); 
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts").then((response) => {;
+      })
+      });
   return <div>{id}</div>
 }
 
