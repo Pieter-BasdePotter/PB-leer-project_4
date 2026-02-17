5673d56dc7a058b16dd4757a3f4a9ee18274a638
Pieter-BasdePottere <pbdepotter@hotmail.com>
2026-02-17 20:23:09 +0100

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Home from './pages/Home';
 import Createpost from './pages/CreatePost';
+import Post from './pages/Post';
 
 
 function App() {
-  return <div className="App"> 
-  <Router>
-    <Link to="/createpost"> Create A post</Link>
-    <Link to="/"> Home Page</Link>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/createpost" element={<Createpost />} />
-    </Routes>
-  </Router></div>;
+  return (
+    <div className="App"> 
+      <Router>
+        <div className="navbar">
+          <Link to="/createpost"> Create A post</Link>
+          <Link to="/"> Home Page</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<Createpost/>} />
+          <Route path="/post/:id" element={<Post/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import axios from "axios";  
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
     const [listOfPosts, setListOfPosts] = useState([]);
+    const navigate = useNavigate();
 
   useEffect(() => {
     axios.get("http://localhost:3001/posts")
@@ -18,7 +20,7 @@ function Home() {
     <div>
       {listOfPosts.map((value) => {
         return (
-          <div className='post' key={value.id}>
+          <div className='post' onClick={() => {navigate(`/post/${value.id}`)}}>
             <div className='title'>{value.title}</div>
             <div className='body'>{value.postText}</div>
             <div className='footer'>{value.userName}</div>
